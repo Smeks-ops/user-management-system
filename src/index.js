fee283fc2a3873d873a305aadfbdd042599133bc
Osemeke Echenim <echenim.osemeke@gmail.com>
2021-07-18 15:09:49 +0100

    connect to database

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import cors from "cors";
+import { Consola } from "consola";
+import express from "express";
+import mongoose from "mongoose";
+import { json } from "body-parser";
+
+//IMPORT APPLICATION CONSTANTS
+import { DB, PORT } from './constants'
+
+// initialize express application
+const app = express();
+
+//Apply application middlewares
+app.use(cors())
+app.use(json())
+
+
+const main = async () => {
+  try {
+    // Connect with the database
+    await mongoose.connect(DB, {
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useNewUrlParser: true,
+    })
+    consola.success("DATABASE CONNECTED...")
+    //start app listening for req on server
+    app.listen(PORT, () => consola.success(`Server started on port ${PORT}`))
+  } catch (error) {
+    consola.error(`Unable to start the server \n${error.message}`)
+  }
+}
+
+main();
